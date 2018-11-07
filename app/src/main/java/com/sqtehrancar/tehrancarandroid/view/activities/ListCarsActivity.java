2038350be7bc578f2f430cef3d63420e61c65e0e
bardia_kzzzz <bardiakarimi77@yahoo.com>
2018-11-07 16:11:33 +0330

    Login Page Logic
    Login Page connect to the server & start new activity after login

---
+++ app/src/main/java/com/sqtehrancar/tehrancarandroid/view/activities/ListCarsActivity.java
@@ -0,0 +1,15 @@
+package com.sqtehrancar.tehrancarandroid.view.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sqtehrancar.tehrancarandroid.R;
+
+public class ListCarsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_car);
+    }
+}
---
+++ app/src/main/java/com/sqtehrancar/tehrancarandroid/view/activities/ListCarsAdminActivity.java
@@ -0,0 +1,13 @@
+package com.sqtehrancar.tehrancarandroid.view.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListCarAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_car_admin);
+    }
+}
