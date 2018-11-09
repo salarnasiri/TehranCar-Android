c066bf20d9a29bd614433946f32f50260ce1ab5c
bardia_kzzzz <bardiakarimi77@yahoo.com>
2018-11-09 11:24:41 +0330

    Added ResponseJson Class
    ResponseJson Class is used for getting status from server

---
+++ app/src/main/java/com/sqtehrancar/tehrancarandroid/model/ResponseJson.java
@@ -0,0 +1,9 @@
+package com.sqtehrancar.tehrancarandroid.model;
+
+public class ResponseJson {
+    final String status;
+
+    public ResponseJson(String status) {
+        this.status = status;
+    }
+}
